fcf27f05b54b6c77d27aac6bb81ad4500116a1bf
Mechasparrow <mikipux7@gmail.com>
2019-01-24 06:56:50 -0600

    stubbed api interface

---
+++ src/app/services/api-interface.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ApiInterfaceService {
+  constructor(private http: HttpClient) {}
+
+  // STUB
+  // sign In
+
+  // sign Out
+
+  // register
+
+  // STUB accounts
+}
