5ca8a0850458666817e6816cc685b885b11b04f8
Mechasparrow <mikipux7@gmail.com>
2019-02-27 08:50:54 -0600

    experimenting with multi select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from "@angular/forms";
+import { NgSelectModule } from "@ng-select/ng-select";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { EditProfilePageComponent } from './edit-profile-page/edit-profile-page.component';
-import { OfferPageComponent } from './offer-page/offer-page.component';
-import { CreatePrintingOfferPageComponent } from './create-printing-offer-page/create-printing-offer-page.component';
-import { CreateOfferSpecPageComponent } from './create-offer-spec-page/create-offer-spec-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { RegisterPageComponent } from "./register-page/register-page.component";
+import { ProfilePageComponent } from "./profile-page/profile-page.component";
+import { EditProfilePageComponent } from "./edit-profile-page/edit-profile-page.component";
+import { OfferPageComponent } from "./offer-page/offer-page.component";
+import { CreatePrintingOfferPageComponent } from "./create-printing-offer-page/create-printing-offer-page.component";
+import { CreateOfferSpecPageComponent } from "./create-offer-spec-page/create-offer-spec-page.component";
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { CreateOfferSpecPageComponent } from './create-offer-spec-page/create-of
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgSelectModule,
     FormsModule
   ],
   providers: [],
