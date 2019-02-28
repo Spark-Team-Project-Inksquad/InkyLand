6875f8dffbe93870c0f02b623e71e9442563ac45
Mechasparrow <mikipux7@gmail.com>
2019-02-27 21:46:06 -0600

    added search-box

--- src/app/search-offers-page/search-offers-page.component.ts
+++ src/app/search-offers-page/search-offers-page.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-search-offers-page',
-  templateUrl: './search-offers-page.component.html',
-  styleUrls: ['./search-offers-page.component.scss']
+  selector: "app-search-offers-page",
+  templateUrl: "./search-offers-page.component.html",
+  styleUrls: ["./search-offers-page.component.scss"]
 })
 export class SearchOffersPageComponent implements OnInit {
+  public query: string = "";
+  public results: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  //conducts a search query
+  search() {
+    alert("search query: " + this.query);
+
+    // NOTE just grabs all the printing offers
   }
-
 }
