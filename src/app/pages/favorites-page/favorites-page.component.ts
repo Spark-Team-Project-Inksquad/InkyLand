3c77a3c37eefdb0ce7f1f8d96c944146bae96759
Mechasparrow <mikipux7@gmail.com>
2019-03-24 21:03:28 -0500

    basic rendering of favorites page

--- src/app/pages/favorites-page/favorites-page.component.ts
+++ src/app/pages/favorites-page/favorites-page.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-favorites-page',
-  templateUrl: './favorites-page.component.html',
-  styleUrls: ['./favorites-page.component.scss']
+  selector: "app-favorites-page",
+  templateUrl: "./favorites-page.component.html",
+  styleUrls: ["./favorites-page.component.scss"]
 })
 export class FavoritesPageComponent implements OnInit {
+  private favoriteVendors: any[] = [];
 
-  constructor() { }
+  //Generates a list of dummy vendors
+  generateDummyVendors(amount: number) {
+    let dummy_vendors: any[] = [];
 
-  ngOnInit() {
+    let colors = ["pink", "yellow", "blue", "black"];
+
+    let dummyVendor = {
+      name: "dummy vendor"
+    };
+
+    for (let i = 0; i < amount; i++) {
+      let vendor_color = colors[i % colors.length];
+      let new_vendor = Object.assign({ color: vendor_color }, dummyVendor);
+      dummy_vendors.push(new_vendor);
+    }
+
+    return dummy_vendors;
   }
 
+  //generates 2 dimension version of vendors list
+  getVendorGrid(items_per_row: number) {
+    let vendors = this.favoriteVendors;
+
+    let rows = Math.ceil(vendors.length / items_per_row);
+    let vendor_grid: any = [];
+    let vendor_i = 0;
+    for (let y = 0; y < rows; y++) {
+      let vendor_row: any[] = [];
+      for (let x = 0; x < items_per_row; x++) {
+        if (vendor_i == vendors.length) {
+          break;
+        }
+        vendor_row.push(vendors[vendor_i]);
+        vendor_i++;
+      }
+      vendor_grid.push(vendor_row);
+    }
+    return vendor_grid;
+  }
+
+  constructor() {}
+
+  ngOnInit() {
+    this.favoriteVendors = this.generateDummyVendors(6);
+  }
 }
