b9228f6bbb087c12f7c3af1218cbf7c521968965
Mechasparrow <mikipux7@gmail.com>
2019-04-04 06:48:18 -0500

    added basic message profile card

--- src/app/sub-components/message-profile-card/message-profile-card.component.ts
+++ src/app/sub-components/message-profile-card/message-profile-card.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-message-profile-card',
-  templateUrl: './message-profile-card.component.html',
-  styleUrls: ['./message-profile-card.component.scss']
+  selector: "message-profile-card",
+  templateUrl: "./message-profile-card.component.html",
+  styleUrls: ["./message-profile-card.component.scss"]
 })
 export class MessageProfileCardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
