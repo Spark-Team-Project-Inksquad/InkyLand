e17f167c654d5a11458340d8a72c39704ea88e6f
mechasparrow <mikipux7@gmail.com>
2019-04-22 13:19:36 -0500

    completed price filter aesthetic

--- src/app/sub-components/filter-popovers/price-filter/price-filter.component.ts
+++ src/app/sub-components/filter-popovers/price-filter/price-filter.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-price-filter',
+  selector: 'price-popup-filter',
   templateUrl: './price-filter.component.html',
   styleUrls: ['./price-filter.component.scss']
 })
 export class PriceFilterComponent implements OnInit {
+
+  @Input('for') for_id:string;
 
   constructor() { }
 
--- src/notes.md
+++ src/notes.md
@@ -7,5 +7,11 @@ Popups:
 https://getbootstrap.com/docs/4.3/components/popovers/
 https://codepen.io/diqidoq/pen/RRojxQ
 
+
 Flexbox for Chat:
 https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+
+# Bugs:
+- [ ] Input field bug on login/signup modals
+- [ ] Buttons sometimes require 2 clicks. IDK why?
