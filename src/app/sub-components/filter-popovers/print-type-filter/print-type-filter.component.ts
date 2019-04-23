6c4b667de47cd35a854e50834e99d62a40fd453a
Mechasparrow <mikipux7@gmail.com>
2019-04-22 22:41:04 -0500

    filter popover aesthetic complete

--- src/app/sub-components/filter-popovers/print-type-filter/print-type-filter.component.ts
+++ src/app/sub-components/filter-popovers/print-type-filter/print-type-filter.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'print-type-popup-filter',
-  templateUrl: './print-type-filter.component.html',
-  styleUrls: ['./print-type-filter.component.scss']
+  selector: "print-type-popup-filter",
+  templateUrl: "./print-type-filter.component.html",
+  styleUrls: ["./print-type-filter.component.scss"]
 })
 export class PrintTypeFilterComponent implements OnInit {
+  //input connector for correct popup connection
+  @Input("for") for_id: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
